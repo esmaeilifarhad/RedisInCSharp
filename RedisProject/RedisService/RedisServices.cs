640c521e62bd72dc6dd00b63362c7ed11608585b
Farhad Esmaeili <f.esmaeili@enb.local>
2021-09-08 12:59:20 +0430

    first

---
+++ RedisProject/RedisService/RedisServices.cs
@@ -0,0 +1,21 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisProject.RedisService
+{
+    public class RedisServices
+    {
+        public void x() {
+            using (var connection = new RedisClient()) {
+                //connection.Set("13","toplearn");
+                var result= connection.Get("13");
+                var stringresult = ASCIIEncoding.ASCII.GetString(result);
+                Console.WriteLine(stringresult);
+            }
+        }
+    }
+}
---
+++ RedisProject/Repositories/PersonRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisProject.Repositories
+{
+    public class PersonRepository
+    {
+        private readonly List<Person> _persons;
+        public PersonRepository()
+        {
+            _persons = new List<Person>() { 
+            new Person{ 
+            Id="100",
+            FullName="Farhad Esmaeili"
+            },
+             new Person{
+            Id="101",
+            FullName="Elahe MalekNejad"
+            },
+              new Person{
+            Id="102",
+            FullName="Maya"
+            }
+            };
+        }
+
+        public Person GetPersonById(string id)
+        {
+            return _persons.Where(q => q.Id == id).FirstOrDefault();
+        }
+    }
+    public class Person {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+    }
+}

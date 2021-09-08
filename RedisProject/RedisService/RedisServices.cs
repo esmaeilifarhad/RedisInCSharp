0c422a6592770b28c7d71f3413dc9366af1a68da
Farhad Esmaeili <f.esmaeili@enb.local>
2021-09-08 13:41:48 +0430

    developer

--- RedisProject/RedisService/RedisServices.cs
+++ RedisProject/RedisService/RedisServices.cs
@@ -1,4 +1,5 @@
-﻿using ServiceStack.Redis;
+﻿using RedisProject.Repositories;
+using ServiceStack.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,47 @@ namespace RedisProject.RedisService
 {
     public class RedisServices
     {
-        public void x() {
-            using (var connection = new RedisClient()) {
+        public void x()
+        {
+
+            var personelId = "100";
+            //do
+            //{
+            //    Console.Write("please inter person number : ");
+            //    //  personelId = Console.ReadLine();
+            //    personelId = "100";
+            //}
+            //while (personelId != "0");
+
+
+            using (var connection = new RedisClient())
+            {
+
+                var result = connection.Get(personelId);//ASCIIEncoding.ASCII.GetString(connection.Get(personelId));
+                if (result != null)
+                {
+                    Console.WriteLine("Person is : ", ASCIIEncoding.ASCII.GetString(result));
+                    Console.WriteLine("this data Come from cache");
+                }
+                else
+                {
+                    var repo = new PersonRepository();
+                    var repoResult = repo.GetPersonById(personelId);
+
+                    connection.Set(personelId, repoResult.FullName);
+                    Console.WriteLine("Person is : ", repoResult.FullName);
+                    Console.WriteLine("this data come from database");
+
+                }
                 //connection.Set("13","toplearn");
-                var result= connection.Get("13");
-                var stringresult = ASCIIEncoding.ASCII.GetString(result);
+                var result2 = connection.Get("13");
+                var stringresult = ASCIIEncoding.ASCII.GetString(result2);
                 Console.WriteLine(stringresult);
+
+
             }
+
+
         }
     }
 }
